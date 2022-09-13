de479cc1cdd4a78def63ffe2a39ea42c9f944748
Eunice js <90851478+Eunice2000@users.noreply.github.com>
2022-09-12 18:45:47 -0700

    full project

--- main.js
+++ main.js
@@ -1,24 +1,9 @@
-// let swiper = new swiper(".mySwiper", {
-//     spaceBetween: 10,
-//     slidesPerView: 4,
-//     freeMode: true,
-//     watchSlidesProgress: true,
-//    });
+var swiper = new Swiper(".mySwiper", {
+    slidesPerView: 3,
+    spaceBetween: 30,
    
-   
-//    let swiper2 = new Swiper(".mySwiper2", {
-//     spaceBetween: 10,
-//     navigation: {
-//       nextEl: ".swiper-button-next",
-//       prevEl: ".swiper-button-prev",
-//     },
-//     thumbs: {
-//       swiper: swiper,
-//     },
-//    });
-var splide = new Splide( '.splide', {
-  type   : 'loop',
-  padding: '5rem',
-} );
-
-splide.mount();
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+  });
---
+++ toggle.js
@@ -0,0 +1,17 @@
+function showpassword(){
+    var x = document.getElementById("passkey");
+    var y = document.getElementById("eye1");
+    var z = document.getElementById("eye2");
+    
+        if(x.type === 'password'){
+            x.type = 'text';
+            y.style.display = 'none';
+            z.style.display = 'block';
+        }
+
+        else{
+            x.type = 'password';
+            y.style.display = 'block';
+            z.style.display = 'none';
+        }
+}
